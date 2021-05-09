25005bd264a14949279007402b89c5833950dae4
Kyle Williams <williams.kyleg@gmail.com>
2021-05-09 13:57:03 -0400

    server port change was not pushed in last commit, delete client README

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React and get guud!
+          Learn React and get guud mutha!
         </a>
       </header>
     </div>
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 5000;
 
 const bodyParser = require('body-parser');
 const axios = require('axios');
