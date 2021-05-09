c14fc0e4ef97626a7f6c56f940fce6537f281b56
Kyle Williams <williams.kyleg@gmail.com>
2021-05-09 14:20:41 -0400

    minor edit

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React and get guud mutha!
+          Learn React and get guud!
         </a>
       </header>
     </div>
