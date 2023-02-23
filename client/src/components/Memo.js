5d2c79534e965115a3bab27cd1800f6dec568c8f
Kyle Williams <kylewilliams@Kyles-MBP.lan1>
2023-02-23 13:56:04 -0500

    line item text and memo reformat

--- client/src/components/Memo.js
+++ client/src/components/Memo.js
@@ -1,22 +1,22 @@
-import { Fragment } from 'react';
+import { Fragment } from "react";
 
-import Form from 'react-bootstrap/Form';
+import Form from "react-bootstrap/Form";
 
 function Memo({ dispatch }) {
-  const handleBlur = event => {
-    dispatch({ type: 'UPDATE_MEMO', payload: event.target.value });
+  const handleBlur = (event) => {
+    dispatch({ type: "UPDATE_MEMO", payload: event.target.value });
   };
 
   return (
     <Fragment>
-      <Form.Group controlId='memo-text-area'>
-        <Form.Label>Memo</Form.Label>
+      <Form.Group controlId="memo-text-area">
+        <Form.Label className="light-blue fw-bold">Memo</Form.Label>
         <Form.Control
-          as='textarea'
-          placeholder='Enter your memo...'
+          as="textarea"
+          placeholder="Enter your memo..."
           cols={2}
           rows={3}
-          onBlur={event => handleBlur(event)}
+          onBlur={(event) => handleBlur(event)}
         />
       </Form.Group>
     </Fragment>
--- seeds/seed.js
+++ seeds/seed.js
@@ -1,38 +1,38 @@
 module.exports = lineItemsSeed = [
   {
-      "details": "a simple 10% off discount",
-      "price": 10,
-      "quantity": 1,
-      "total": 10
+    details: "A simple 10% off discount",
+    price: 10,
+    quantity: 1,
+    total: 10,
   },
   {
-      "details": "catering service add on - extra personnel",
-      "price": 345,
-      "quantity": 1,
-      "total": 345
+    details: "Catering service add on - extra personnel",
+    price: 345,
+    quantity: 1,
+    total: 345,
   },
   {
-      "details": "full catering service",
-      "price": 1249.99,
-      "quantity": 1,
-      "total": 1249.99
+    details: "Full catering service",
+    price: 1249.99,
+    quantity: 1,
+    total: 1249.99,
   },
   {
-      "details": "This is a test item by matt to test this API",
-      "price": 1.5,
-      "quantity": 1,
-      "total": 1.5
+    details: "Custom event RSVP card",
+    price: 1.5,
+    quantity: 1,
+    total: 1.5,
   },
   {
-      "details": "hourly rate for equipment rental",
-      "price": 62.75,
-      "quantity": 1,
-      "total": 62.75
+    details: "Hourly rate for equipment rental",
+    price: 62.75,
+    quantity: 1,
+    total: 62.75,
   },
   {
-      "details": "5 gal bucket apples",
-      "price": 34.55,
-      "quantity": 1,
-      "total": 34.55
-  }
-]
+    details: "5 gal bucket of apples",
+    price: 34.55,
+    quantity: 1,
+    total: 34.55,
+  },
+];
